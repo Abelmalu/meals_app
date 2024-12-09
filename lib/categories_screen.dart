c287a7f6c7991b2070096deff36e10c58a77c570
abel malu <abellmalu@gmail.com>
2024-12-09 10:44:24 +0300

    categoris screen and dummy data done

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,9 +6,14 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
+      children: <Widget>[
+        
+      ],
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
+        childAspectRatio: 3/2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20
       ),
     );
   }
---
+++ lib/dummy_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'models/categories_model.dart';
+
+List DUMMY_CATEGORIES = [
+  CategoryRecipe(
+    id: 'c1',
+    title: 'Ethiopian',
+    color: Colors.purple,
+  ),
+  CategoryRecipe(
+    id: 'c2',
+    title: 'Quic and Easy',
+    color: Colors.green,
+  ),
+  CategoryRecipe(
+    id: 'c3',
+    title: 'Vegetabales',
+    color: Colors.blue,
+  ),
+  CategoryRecipe(
+    id: 'c4',
+    title: 'carnivore',
+    color: Colors.blue,
+  ),
+];
